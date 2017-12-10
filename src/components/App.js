05722f1c0e42ea51fe925ff237f2a1e7c238a5fd
Francis Yang <xyang42@ucsc.edu>
2017-12-10 00:07:05 -0800

    Able to add or delete reminders

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addReminder, deleteReminder } from "../actions";
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+				text: ''
+			}
+		}
+
+	addReminder() {
+		console.log('this: ', this)
+		this.props.addReminder(this.state.text)
+	}
+
+	deleteReminder(id) {
+		this.props.deleteReminder(id)
+	}
+
+	renderReminders() {
+		const { reminders } = this.props
+		return (
+				<ul className="list-group col-sm-4">
+					{
+						reminders.map(reminder => {
+							return (
+									<li key={reminder.id} className="list-group-item">
+										<div className="list-item">{reminder.text}</div>
+										<div className="list-item delete-button"
+												onClick={() => this.deleteReminder(reminder.id)}>
+											&#x2715;
+										</div>
+									</li>
+							)
+						})
+					}
+				</ul>
+		)
+	}
+
 	render() {
 		return (
 				<div className = "App">
 					<div className = "title">
 						Reminder Pro
 					</div>
-					<div className="form-inline">
+					<div className="form-inline reminder-form">
 						<div className="form-group">
-							<input className="form-contrl" placeholder="I have to..."/>
+							<input className="form-contrl" placeholder="I have to..."
+										onChange={event => this.setState({text: event.target.value})}/>
 						</div>
-						<button type="button" className="btn btn-success">Add Reminder</button>
+						<button type="button" className="btn btn-success"
+										onClick={() => this.addReminder()}>
+							Add Reminder
+						</button>
 					</div>
+					{ this.renderReminders() }
 				</div>
 		)
 	}
 }
 
-export default App;
+function mapStateToProps(state) {
+	return {
+		reminders: state
+	}
+}
+
+export default connect(mapStateToProps, { addReminder, deleteReminder })(App);
